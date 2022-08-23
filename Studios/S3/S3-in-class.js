7fd585b2d6c6ce5b8c69e9e093b63f5e092f1ed3
Source Academy User <No public email provided>
2022-08-23 12:56:42 +0800

    Changes made from Source Academy

--- Studios/S3/S3-in-class.js
+++ Studios/S3/S3-in-class.js
@@ -1,10 +1,32 @@
 import {circle, show, stack, stack_frac, beside, beside_frac,
-        ribbon, square} from "rune";
+        ribbon, square, blank, scale} from "rune";
 
 function moony_1(bottom_right){
     
-    return beside(stack(circle, square), bottom_right);
+    return beside(stack(circle, square), stack(blank, bottom_right));
     
 }
 
-show(moony_1);
+//show(moony_1(ribbon)); //Q1
+
+function moony_2(n){
+    
+    return n === 1 ? moony_1(circle)
+                   : moony_1(moony_2(n - 1));
+}
+
+// show(moony_2(5)); //Q2
+
+function moony_1_mod(bottom_right, n){
+    
+    return beside_frac(1/n ,stack(circle, square), stack(blank, bottom_right));
+    
+}
+
+function moony(n){
+    
+    return n === 3 ? moony_1_mod(circle, n)
+                   : moony_1_mod(moony_2(n - 1), n);
+}
+
+show(moony(4)); //Q3
