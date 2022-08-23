c7964e7f95bd9be91710100f6103045c351e88ac
Source Academy User <No public email provided>
2022-08-23 12:42:31 +0800

    Changes made from Source Academy

---
+++ Studios/S3/S3-in-class.js
@@ -0,0 +1,10 @@
+import {circle, show, stack, stack_frac, beside, beside_frac,
+        ribbon, square} from "rune";
+
+function moony_1(bottom_right){
+    
+    return beside(stack(circle, square), bottom_right);
+    
+}
+
+show(moony_1)
