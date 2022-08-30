351f49d98a35975b68176ba1c2c7937389389d5c
Source Academy User <No public email provided>
2022-08-30 13:19:39 +0800

    Changes made from Source Academy

--- Studios/S4/S4-home.js
+++ Studios/S4/S4-home.js
@@ -1,2 +1,26 @@
-const pascal = (n, m) => n * m * (n - m) === 0 ? 1
-    : pascal(n - 1, m  - 1) + pascal (n - 1, m);
+//Q1
+//a. 1
+//b. 1
+
+//Q2
+//2
+
+//Q3
+//27
+
+const compose = (f, g) => x => f(g(x));
+const repeated = (f, n) => n === 0 ? x => x
+                                   : x => f(repeated(f, n - 1));
+const thrice = f => repeated(f, 3);
+
+//Q4
+//a. 6 + 27
+//b. this returns the compose function
+//c. 1
+//d. 2 ** (512 ** 3)
+
+const square = x => x * x;
+
+(thrice(thrice))(x => x + 1)(7);
+
+thrice(x => x + 1);
