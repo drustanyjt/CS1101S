1850539e60ffe5a99237b122eb810b40db38a72c
Source Academy User <No public email provided>
2022-09-13 11:02:03 +0800

    Changes made from Source Academy

--- Studios/S6/S6.js
+++ Studios/S6/S6.js
@@ -9,16 +9,16 @@ function remove_duplicates(xs) {
 function makeup_amount(x, coins) {
     if (x === 0) {
         return list(null);
-            } else if (x < 0 || is_null(coins)) {
-                return null;
-            } else {
-                // Combinations that do not use the head coin.
-                const combi_A = makeup_amount(x, tail(coins));
-                // Combinations that do not use the head coin
-                // for the remaining amount.
-                const combi_B = makeup_amount(x - head(coins), tail(coins));
-                // Combinations that use the head coin.
-                const combi_C = map(xs => pair(head(coins), xs), combi_B);
+    } else if (x < 0 || is_null(coins)) {
+        return null;
+    } else {
+        // Combinations that do not use the head coin.
+        const combi_A = makeup_amount(x, tail(coins));
+        // Combinations that do not use the head coin
+        // for the remaining amount.
+        const combi_B = makeup_amount(x - head(coins), tail(coins));
+        // Combinations that use the head coin.
+        const combi_C = map(xs => pair(head(coins), xs), combi_B);
         return append(combi_A, combi_C);
     }
 }
