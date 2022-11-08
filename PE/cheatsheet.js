1a98ddf309122aa7499d4e86da906585de95034d
Source Academy User <No public email provided>
2022-11-08 16:33:27 +0800

    Changes made from Source Academy

--- PE/cheatsheet.js
+++ PE/cheatsheet.js
@@ -35,13 +35,42 @@ function array_append(a, b) {
 
 array_append(arr, arr);
 
-let num_stack = list();
+
+
+function make_stack(){
+    let stack = list();
     
-function pop() {
-    const res = head(num_stack);
-    num_stack = tail(num_stack);
-    return res;
+    function pop() {
+        const res = head(stack);
+        stack = tail(stack);
+        return res;
+    }
+    function push(n) {
+        stack = pair(n, stack);
+    }
+    
+    return [() => stack, pop, push];
 }
-function push(n) {
-    num_stack = pair(n, num_stack);
+const s_p_p = make_stack();
+s_p_p[2](2);
+s_p_p[1]();
+
+function make_Q(){
+    let queue = list();
+    
+    function enqueue(n){
+        queue = append(queue, list(n));
+    }
+    function dequeue(){
+        const res = head(queue);
+        queue = tail(queue);
+        return res;
+    }
+    
+    return [() => queue, dequeue, enqueue];
 }
+
+const q_d_e = make_Q();
+q_d_e[2](2);
+display(q_d_e[0]());
+q_d_e[1]();
